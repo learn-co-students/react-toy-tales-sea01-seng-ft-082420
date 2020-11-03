07f8aa4f6723a45a7b8bc24051b8b457451e5d5e
JacksonBey <jacksongrowson@gmail.com>
2020-11-03 13:52:10 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
+const URL = 'http://localhost:3000/toys'
+
 
 class App extends React.Component{
 
   state = {
-    display: false
+    display: false,
+    toys: []
   }
 
   handleClick = () => {
@@ -19,20 +22,66 @@ class App extends React.Component{
     })
   }
 
+  componentDidMount() {
+    fetch(URL)
+    .then(res => res.json())
+    .then(toys => {
+      this.setState({
+        toys
+      })
+    })
+  }
+
+
+  handleDonate = (toy) => {
+    this.setState({
+      toys: this.state.toys.filter(t => (t.id !== toy.id))
+    })
+    fetch((URL + `/${toy.id}`), {
+      method: "DELETE"
+    })
+  }
+
+  addToy = (toy, e) => {
+    e.preventDefault()
+    this.handleClick()
+    let ntoy;
+    
+    fetch((URL), {
+      method: "POST",
+      headers:  {
+        Accept: 'application/json',
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: toy.name,
+        image: toy.image,
+        likes: 0
+      })
+    })
+    .then(res => res.json())
+    .then(toy => 
+      this.setState({
+        toys: [...this.state.toys, toy]
+      })
+    
+      )
+  }
+
   render(){
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm addToy={this.addToy}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} handleDonate ={this.handleDonate}/>
       </>
     );
   }
--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+const ToyContainer = (props) => {
+
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {console.log(props.toys),
+      props.toys.map((toy, idx) => <ToyCard key={idx} toy={toy} handleDonate={props.handleDonate}/>)
+}
     </div>
   );
+}
+ToyContainer.defaultProps = {
+  toys: []
 }
 
 export default ToyContainer;
